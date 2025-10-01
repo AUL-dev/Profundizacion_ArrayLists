98ce1f8bd30b018478cf0b9fb5c2daef1ca586ef
Alberto <alberto.uria.lozoya@gmail.com>
2025-10-01 16:02:58 +0200

    Ejercicios de práctica

--- src/main/java/EjercicioCRUD/Main.java
+++ src/main/java/EjercicioCRUD/Main.java
@@ -7,9 +7,13 @@ public class Main {
     public static void main(String[] args) {
         Connection conexion = ConexionCiudad.abrirConexion();
         ArrayList<EntityCity> creacionArray = ConsultaBBDD.creacion(conexion);
-        ConsultaBBDD.leerTodos(creacionArray);
-        ConsultaBBDD.actualiza(creacionArray,4078,4079,"Rufus","RUF", "Rufus",32);
-        ConsultaBBDD.elimina(creacionArray,4078);
+        // ConsultaBBDD.leerTodos(creacionArray);
+        //ConsultaBBDD.actualiza(creacionArray, 4078, 4079, "Rufus", "RUF", "Rufus", 32);
+        //ConsultaBBDD.elimina(creacionArray,4078);
 
+        creacionArray.stream()
+                .distinct()
+                .filter(pais -> pais.getPoblacion() > 1000000 && pais.getNombre().length() > 7 && pais.getNombre().charAt(0) == 'V')
+                .forEach(pais -> System.out.println("El país es: " + pais));
     }
 }
